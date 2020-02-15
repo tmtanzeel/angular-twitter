80ebde4fc253b7c7779fff74947ade8aa1d60a30
tmtanzeel <tmtanzeel9@gmail.com>
2020-02-16 03:46:14 +0530

    added code for routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TwitterTimelineComponent } from './twitter-timeline/twitter-timeline.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'twitter_timeline', component: TwitterTimelineComponent },
+  { path: '', pathMatch: 'full' },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
