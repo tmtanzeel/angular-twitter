84829ee1a140b82fa6ac1a84ea9ebe29403afa87
tmtanzeel <tmtanzeel9@gmail.com>
2020-02-16 02:48:10 +0530

    supplied api keys to twit package

--- server/server.js
+++ server/server.js
@@ -3,3 +3,10 @@ const Twitter = require('twit');
 const app = express();
 
 app.listen(3000, () => console.log('Server running'));
+
+const api_client = new Twitter({
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: ''
+})
