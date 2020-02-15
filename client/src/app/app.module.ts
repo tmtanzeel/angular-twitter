78d3d626421a108051fa283fc158b7fbeb12ca32
tmtanzeel <tmtanzeel9@gmail.com>
2020-02-16 03:38:28 +0530

    created a component for displaying tweets

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TwitterTimelineComponent } from './twitter-timeline/twitter-timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TwitterTimelineComponent
   ],
   imports: [
     BrowserModule,
