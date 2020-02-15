00e639ba76b07c7ae15318c3984dee692c5ccd97
tmtanzeel <tmtanzeel9@gmail.com>
2020-02-16 03:53:24 +0530

    added code for pulling data from twitter service

--- client/src/app/twitter-timeline/twitter-timeline.component.ts
+++ client/src/app/twitter-timeline/twitter-timeline.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { TwitterserviceService } from '../twitterservice.service';
+
 
 @Component({
   selector: 'app-twitter-timeline',
   templateUrl: './twitter-timeline.component.html',
   styleUrls: ['./twitter-timeline.component.scss']
 })
+
+
 export class TwitterTimelineComponent implements OnInit {
-
-  constructor() { }
-
+   
+  myTimeline: any;
+ 
+  constructor(private api: TwitterserviceService) { }
+ 
   ngOnInit() {
+   this.getTwitterTimeline();
   }
-
+   
+  getTwitterTimeline(): void {
+    this.api.getTimeline()
+      .subscribe(
+        myTimeline => {
+          this.myTimeline = myTimeline;
+          console.log(this.myTimeline);
+        }
+      )
+   }
+   
 }
