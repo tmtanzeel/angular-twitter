2fcbe33b6305af53ac2e9f8d01e8cc9ba3872083
tmtanzeel <tmtanzeel9@gmail.com>
2020-02-16 03:34:07 +0530

    added code for getting timeline tweets

--- client/src/app/twitterservice.service.ts
+++ client/src/app/twitterservice.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TwitterserviceService {
 
-  constructor() { }
+  api_url = 'https:/localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  getTimeline() {
+    return this.http
+      .get<any[]>(this.api_url+'/home_timeline')
+      .pipe(this.map(data => data));
+  }
 }
