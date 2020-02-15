93b42a1f0e8f648113eed2562be80f5e9a53b6bc
tmtanzeel <tmtanzeel9@gmail.com>
2020-02-16 02:50:18 +0530

    supplied values to the api keys for twit package

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const app = express();
 app.listen(3000, () => console.log('Server running'));
 
 const api_client = new Twitter({
-    consumer_key: '',
-    consumer_secret: '',
-    access_token: '',
-    access_token_secret: ''
+    consumer_key: '5KvPZUZ83y8uPDJypf1zLAplc',
+    consumer_secret: 'm1sfnBIv50gnIOn8addb1MzBmUyi1Ml9MeAFfhPVAet1WMha6Z',
+    access_token: '1219680138939359232-0Tgj5k3KjQZnvbBy7roAPKBpk0eNJB',
+    access_token_secret: 'Wc57BqdPSqhJWqABpv2j57wYLN1pjIkrDJUNVdLnoWJmx'
 })
